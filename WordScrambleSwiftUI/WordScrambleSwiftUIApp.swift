70fe62419547d42fe05e13f978949b5ee5fe3ba2
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-03-07 12:15:56 -0700

    Initial Commit

---
+++ WordScrambleSwiftUI/WordScrambleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleSwiftUIApp.swift
+//  WordScrambleSwiftUI
+//
+//  Created by Mitch Andrade on 3/7/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
